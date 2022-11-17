34cecc3bb93022c24ed373799d28f0def7c485ba
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-11-17 18:19:54 +0100

    feat: loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,8 @@
+import ReactLoading from 'react-loading';
+
+const Loading = () => {
+    return (
+        <ReactLoading type={"spokes"} color={"#ffffff"} height={567} width={275} className="pt-28" />
+    )
+}
+export default Loading
